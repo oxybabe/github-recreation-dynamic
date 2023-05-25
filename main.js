9736428ecb1180d433bd421b2e8acd73b7f660c5
Elia Partin <arenaselia@gmail.com>
2023-05-25 09:00:00 -0400

    added api url

--- main.js
+++ main.js
@@ -1,33 +1,48 @@
-const getResources = async () => {
-  const request = await fetch("https://api.github.com/users/oxybabe/repos");
-  const response = await request.json();
-  const name = response[0].name;
-  console.log(name);
+const renderRepos = () => {
+  const container = document.querySelector("#repos-list");
+  repos.forEach((repo) => {
+    let listRepo = document
+      .querySelector("#repo-title")
+      .content.cloneNode(true);
+    listRepo.appendChild(document.createTextNode(repo));
+    container.appendChild(listRepo);
+  });
 };
 
-document
-  .getElementById("fetch-resources")
-  .addEventListener("click", getResources);
+const getResources = async () => {
+  const request = await fetch(
+    "https://api.github.com/users/oxybabe/repos?sort=updated_at"
+  );
+  const response = await request.json();
+  response.forEach((obj) => {
+    repos.push(obj.name);
+  });
+  console.log(response);
+  renderRepos();
+};
 
-let repos = []
-async function getRepos() {
-  const response = await fetch("https://api.github.com/users/oxybabe/repos/")
-  const jsonData = await response.json()
-  // console.log(jsonData);
-  jsonData.forEach((obj) => { repos.push(obj.text) })
-  renderRepos()
-}
+let repos = [];
+getResources();
+
+//"user_repositories_url": "https://api.github.com/users/{user}/repos{?type,page,per_page,sort}",
+
+// document
+//   .getElementById("fetch-resources")
+//   .addEventListener("click", getResources);
+
+// async function getRepos() {
+//   const response = await fetch("https://api.github.com/users/oxybabe/repos/");
+//   const jsonData = await response.json();
+//   // console.log(jsonData);
+//   jsonData.forEach((obj) => {
+//     repos.push(obj.text);
+//   });
+//   renderRepos();
+// }
 // getRepos = async function () {
 //   const response = await fetch("https://api.github.com/users/oxybabe/repos/");
 //   console.log(response);
 //   const jsonData = await response.json();
-// }
-
-// const renderRepos = () => {
-//   const container = document.querySelector("#repos-list")
-//   repos.forEach((repos) => {
-//     let listItem = document.querySelector(#repos-list)
-//   })
 // }
 
 // getUser = async function () {
